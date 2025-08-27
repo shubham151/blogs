86ce4abf15c85c00b3e79d1012284280fe51d5a8
Shubham Mishra <shubham.cmishra@gmail.com>
2025-08-26 23:23:12 -0700

    Add home page and blog pages

---
+++ src/pages/BlogPage/BlogPage.jsx
@@ -0,0 +1,7 @@
+import BlogPage from "../../components/Blog/BlogPage"; // Your blog component
+
+const Blog = () => {
+  return <BlogPage />;
+};
+
+export default Blog;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,7 @@
+import HomePage from "../../components/Home/HomePage";
+
+const Blog = () => {
+  return <HomePage />;
+};
+
+export default Blog;
